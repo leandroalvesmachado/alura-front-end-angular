dbc9615e5e113e6cfa078140b867ab1ff5774e07
leandroalvesmachado <leandroalvesmachado@gmail.com>
2021-08-10 15:48:40 -0300

    Salvando

--- angular-boas-praticas-em-arquiteturas-e-formularios/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ angular-boas-praticas-em-arquiteturas-e-formularios/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
 
 import { NovoUsuarioService } from './novo-usuario.service';
 
@@ -11,7 +12,7 @@ import { NovoUsuarioService } from './novo-usuario.service';
 export class NovoUsuarioComponent implements OnInit {
 
   // ! informa que pode ser nulo
-  novoUsuarioForm!: FormGroup
+  novoUsuarioForm!: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -21,7 +22,29 @@ export class NovoUsuarioComponent implements OnInit {
   // ciclo de vida, após a classe injetar todos os servicos
   // após execução do constructor
   ngOnInit(): void {
-    this.novoUsuarioForm = this.formBuilder.group();
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ],
+      fullName: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(4)
+        ]
+      ],
+      userName: [''],
+      password: ['']
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
   }
 
 }
